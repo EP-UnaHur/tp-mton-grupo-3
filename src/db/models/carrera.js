0c6cd58bb34789ffa93381abf33872325a5f5a13
ErikaLois <erikapatricia.lois@estudiantes.unahur.edu.ar>
2024-05-22 20:00:51 -0300

    Creación de modelo Carrera

---
+++ src/db/models/carrera.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Carrera extends Model {
+   
+    static associate(models) {
+      Carrera.hasMany(models.Materia, {
+        as: 'materias',
+        foreignKey: 'materia_id'
+      })
+    }
+  }
+
+  Carrera.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    nombre: DataTypes.STRING,
+    grado: DataTypes.STRING,
+    universidad: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Carrera',
+  });
+
+  return Carrera;
+  
+};
