a5db116163ce95df736f601ec62acb5eabec91de
Francisco Villalba <fj.villalba@outlook.com>
2024-06-03 18:44:04 -0300

    add db port env variable

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DIALECT
+    "dialect": process.env.DIALECT,
     "port": process.env.DB_PORT
 }
 };
